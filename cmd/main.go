088bc6a12b11a34d35db5847e20ad788eca3b6e4
Tyler Shelton <tshelton@polarislabs.com>
2022-09-05 20:15:24 -0400

    finished POST endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -2,28 +2,31 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-
 )
 
-type (
-	user struct {
-		Username int `json:"username"`
-		Password string `json:"password"`
-		Token int `json:"token"`
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Token    int    `json:"token"`
+}
+
+func handleUserValidation(c echo.Context) error {
+	var user User
+	timeStamp := time.Now().Hour()*100 + time.Now().Minute()
+
+	if err := c.Bind(&user); err != nil {
+		return c.String(http.StatusBadRequest, "Bad Request")
 	}
-)
 
-func createUser(c echo.Context) error {
-	// e.Logger.
-	// u: user = c.
-	// if err := c.Bind(u); err != nil {
-	// 	return err
-	// }
+	if user.Username != "c137@onecause.com" || user.Password != "#th@nH@rm#y#r!$100%D0p#" || user.Token != timeStamp {
+		return c.String(http.StatusUnauthorized, "Unauthorized")
+	}
 
-	return c.JSON(http.StatusCreated, '')
+	return c.JSON(http.StatusCreated, "Login Success")
 }
 
 func main() {
@@ -32,7 +35,7 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.POST("/users", createUser)
+	e.POST("/login", handleUserValidation)
 
 	e.Logger.Fatal(e.Start(":3030"))
 }
